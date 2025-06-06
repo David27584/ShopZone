5d588f04775517f320fc32bf69ead48e3ba18ba5
Juan Pardo <juandavidkickbudoski@gmail.com>
2025-06-06 08:54:54 -0500

    Integre una base de datos con mongoose

--- shopzone-backend/config/db.js
+++ shopzone-backend/config/db.js
@@ -1,1 +1,14 @@
+const mongoose = require('mongoose');
 
+const conectarDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+
+        console.log('📡 Conectado a MongoDB');
+    } catch (error) {
+        console.error('❌ Error al conectar a MongoDB:', error.message);
+        process.exit(1);
+    }
+};
+
+module.exports = conectarDB;
